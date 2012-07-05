548fb8cf2ac118798d216ff8e291698f87d7086e
Robert Zuber <rob@zuber.net>
2012-07-05 16:18:50 -0700

    Coerce ruby-style resque classes into usable clj.

--- src/resque_clojure/worker.clj
+++ src/resque_clojure/worker.clj
@@ -1,9 +1,21 @@
 (ns resque-clojure.worker
   (:refer-clojure :exclude [name])
-  (:use [clojure.string :only [split]]))
+  (:require [clojure.string :as s]))
+
+(defn hyphenate [title]
+  "Convert a titlecase'd class or method name (like those used on the ruby resque side) to hyphen-separated lowercase"
+  (s/lower-case (s/replace title #"([a-z])([A-Z])" "$1-$2")))
+
+(defn split-namespace [fn-name]
+  "Split a qualified clojure namespace or convert a fully qualified ruby class name into clojure namespace"
+  (let [[namespace fun] (s/split fn-name #"/")]
+    (if (nil? fun)
+      (let [parts (map hyphenate (s/split namespace #"::"))]
+        [(s/join "." (butlast parts)) (last parts)])
+      [namespace fun])))
 
 (defn lookup-fn [namespaced-fn]
-  (let [[namespace fun] (split namespaced-fn #"/")]
+  (let [[namespace fun] (split-namespace namespaced-fn)]
     (ns-resolve (symbol namespace) (symbol fun))))
 
 (defn work-on [state {:keys [func args queue] :as job}]
@@ -15,6 +27,6 @@
 
 (defn name [queues]
   (let [pid-host (.getName (java.lang.management.ManagementFactory/getRuntimeMXBean))
-        [pid hostname] (split pid-host #"@")
+        [pid hostname] (s/split pid-host #"@")
         qs (apply str (interpose "," queues))]
     (str hostname ".clj:" pid ":" qs)))
