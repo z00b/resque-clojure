b0701589f45abda5053193de01002725ea65be90
Robert Zuber <rob@zuber.net>
2012-07-07 21:49:49 -0700

    Make the lookup-fn a more obvious override.
    Use instead of the default, as opposed to inside it.

--- src/resque_clojure/worker.clj
+++ src/resque_clojure/worker.clj
@@ -5,21 +5,23 @@
 (def config (atom {}))
 
 (defn configure [c]
+  "use the config map entry :lookup-fn to override the default lookup-fn for jobs found in resque
+  ie, (worker/configure {:lookup-fn my-custom-func})
+  where my-custom-func take a single argument of the :class string found in the resque job and returns
+  a function capable of processing the jobs arguments"
   (swap! config merge c))
 
 (defn lookup-fn [namespaced-fn]
-  (let [handler (:lookup-fn @config)]
-    (if (nil? handler)
-      (let [[namespace fun] (split namespaced-fn #"/")]
-        (ns-resolve (symbol namespace) (symbol fun)))
-      (handler namespaced-fn))))
+  (let [[namespace fun] (split namespaced-fn #"/")]
+    (ns-resolve (symbol namespace) (symbol fun))))
 
 (defn work-on [state {:keys [func args queue] :as job}]
-  (try
-    (apply (lookup-fn func) args)
-    {:result :pass :job job :queue queue}
-    (catch Exception e
-      {:result :error :exception e :job job :queue queue})))
+  (let [resolver (or (:lookup-fn @config) lookup-fn)]
+    (try
+      (apply (resolver func) args)
+      {:result :pass :job job :queue queue}
+      (catch Exception e
+        {:result :error :exception e :job job :queue queue}))))
 
 (defn name [queues]
   (let [pid-host (.getName (java.lang.management.ManagementFactory/getRuntimeMXBean))
